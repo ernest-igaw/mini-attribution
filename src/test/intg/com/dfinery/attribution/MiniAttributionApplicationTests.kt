137e2e1742f800451e38fbe3470af7b8cc8d56ab
ernest-igaw <ernest.lee@igaworks.com>
2025-01-20 18:38:02 +0900

    Initial commit

---
+++ src/test/intg/com/dfinery/attribution/MiniAttributionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dfinery.attribution
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MiniAttributionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
