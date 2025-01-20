8e9d794b6bc3e8c474df40c4948bdeb2c3aab53e
ernest-igaw <ernest.lee@igaworks.com>
2025-01-20 18:37:44 +0900

    Initial commit

---
+++ src/main/kotlin/com/dfinery/attribution/MiniAttributionApplication.kt
@@ -0,0 +1,11 @@
+package com.dfinery.attribution
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MiniAttributionApplication
+
+fun main(args: Array<String>) {
+	runApplication<MiniAttributionApplication>(*args)
+}
